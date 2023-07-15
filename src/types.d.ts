1e8ddfc095009b0a5ac429376f3a725eb45d4aad
Helltraitor <helltraitor@hotmail.com>
2023-07-15 18:46:21 +0300

    feat: added types shim

---
+++ src/types.d.ts
@@ -0,0 +1,55 @@
+type RouteString = string
+
+export type FeedRSSContentType = 'application/json' | 'application/atom+xml' | 'application/rss+xml'
+export type FeedRSSType = 'json1' | 'atom1' | 'rss2'
+
+export interface FeedRevisitObject {
+  seconds?: number
+  minutes?: number
+  hours?: number
+  days?: number
+}
+
+type FRQDays = `${number}d`
+type FRQHours = `${number}h`
+type FRQMinutes = `${number}m`
+type FRQSeconds = `${number}s`
+
+type FRQDaysHours = `${FRQDays} ${FRQHours}`
+type FRQDaysHoursMinutes = `${FRQDaysHours} ${FRQMinutes}`
+type FRQDaysHoursMinutesSeconds = `${FRQDaysHoursMinutes} ${FRQSeconds}`
+
+type FRQHoursMinutes = `${FRQHours} ${FRQMinutes}`
+type FRQHoursMinutesSeconds = `${FRQHoursMinutes} ${FRQSeconds}`
+
+type FRQMinutesSeconds = `${FRQMinutes} ${FRQSeconds}`
+
+export type FeedRevisitQuery = (
+  // DAYS
+  FRQDays
+  | FRQDaysHours
+  | FRQDaysHoursMinutes
+  | FRQDaysHoursMinutesSeconds
+  // HOURS
+  | FRQHours
+  | FRQHoursMinutes
+  | FRQHoursMinutesSeconds
+  // MINUTES
+  | FRQMinutes
+  | FRQMinutesSeconds
+  // SECONDS
+  | FRQSeconds
+)
+
+export type FeedRevisit = FeedRevisitQuery | FeedRevisitObject
+
+export interface FeedmeRSSOptions {
+  type?: FeedRSSType
+  revisit?: FeedRevisit
+}
+
+export interface FeedmeModuleOptions {
+  feeds: Record<RouteString, FeedmeRSSOptions | undefined>
+}
+
+export {}
