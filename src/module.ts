b08f6d6d4429322a5a9b8a92ab9f24d64d22980d
Helltraitor <helltraitor@hotmail.com>
2023-07-15 14:15:49 +0300

    fix: eslint fix applied

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,4 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { addPlugin, createResolver, defineNuxtModule } from '@nuxt/kit'
 
 // Module options TypeScript interface definition
 export interface ModuleOptions {}
@@ -6,14 +6,14 @@ export interface ModuleOptions {}
 export default defineNuxtModule<ModuleOptions>({
   meta: {
     name: 'nuxt-feedme',
-    configKey: 'feedme'
+    configKey: 'feedme',
   },
   // Default configuration options of the Nuxt module
   defaults: {},
-  setup (options, nuxt) {
+  setup(options, nuxt) {
     const resolver = createResolver(import.meta.url)
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
     addPlugin(resolver.resolve('./runtime/plugin'))
-  }
+  },
 })
