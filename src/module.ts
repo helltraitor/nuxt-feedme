f9b7b9de4f5051570fe18c6f5791eaec32ed7743
Helltraitor <helltraitor@hotmail.com>
2023-07-15 14:12:37 +0300

    fix: replaced old metadata

--- src/module.ts
+++ src/module.ts
@@ -5,8 +5,8 @@ export interface ModuleOptions {}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule'
+    name: 'nuxt-feedme',
+    configKey: 'feedme'
   },
   // Default configuration options of the Nuxt module
   defaults: {},
