88c5c558f86f49817f59785ea0ed9025048f6623
Helltraitor <helltraitor@hotmail.com>
2023-07-16 02:27:03 +0300

    fix: added several valid variations

--- src/types.d.ts
+++ src/types.d.ts
@@ -10,18 +10,32 @@ export interface FeedRevisitObject {
   days?: number
 }
 
+// Duration primitives
 type FRQDays = `${number}d`
 type FRQHours = `${number}h`
 type FRQMinutes = `${number}m`
 type FRQSeconds = `${number}s`
 
+// Days\Hours\*
 type FRQDaysHours = `${FRQDays} ${FRQHours}`
 type FRQDaysHoursMinutes = `${FRQDaysHours} ${FRQMinutes}`
 type FRQDaysHoursMinutesSeconds = `${FRQDaysHoursMinutes} ${FRQSeconds}`
 
+// Days\Minutes\*
+type FRQDaysMinutes = `${FRQDays} ${FRQMinutes}`
+type FRQDaysMinutesSeconds = `${FRQDaysMinutes} ${FRQSeconds}`
+
+// Days\Seconds
+type FRQDaysSeconds = `${FRQDays} ${FRQSeconds}`
+
+// Hours\Minutes\*
 type FRQHoursMinutes = `${FRQHours} ${FRQMinutes}`
 type FRQHoursMinutesSeconds = `${FRQHoursMinutes} ${FRQSeconds}`
 
+// Hours\Seconds
+type FRQHoursSeconds = `${FRQHours} ${FRQSeconds}`
+
+// Minutes\Seconds
 type FRQMinutesSeconds = `${FRQMinutes} ${FRQSeconds}`
 
 export type FeedRevisitQuery = (
@@ -30,10 +44,14 @@ export type FeedRevisitQuery = (
   | FRQDaysHours
   | FRQDaysHoursMinutes
   | FRQDaysHoursMinutesSeconds
+  | FRQDaysMinutes
+  | FRQDaysMinutesSeconds
+  | FRQDaysSeconds
   // HOURS
   | FRQHours
   | FRQHoursMinutes
   | FRQHoursMinutesSeconds
+  | FRQHoursSeconds
   // MINUTES
   | FRQMinutes
   | FRQMinutesSeconds
