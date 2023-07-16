2fd678c5b3322bf5f3771b5991a4f65246da496f
Helltraitor <helltraitor@hotmail.com>
2023-07-16 23:48:08 +0300

    feat: added title for handle hook
    At this moment branch totally fixes typing issues with opening an issue
    
    NOTE: opened issue https://github.com/unjs/hookable/issues/87

--- playground/server/plugins/feed.ts
+++ playground/server/plugins/feed.ts
@@ -1,7 +1,7 @@
 import type { NitroApp } from 'nitropack'
 
 export default (nitroApp: NitroApp) => {
-  nitroApp.hooks.hook('feedme:handle', async ({ feed }) => {
-    return feed.create({ id: '', title: '', copyright: '' })
+  nitroApp.hooks.hook('feedme:handle', async ({ context: { event }, feed }) => {
+    return feed.create({ id: '', title: `Feed for '${event.path}' route`, copyright: '' })
   })
 }
