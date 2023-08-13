a26ec4ec68068417c3a6774da79b0d78205e0ee0
Helltraitor <helltraitor@hotmail.com>
2023-08-13 13:56:10 +0300

    feat: extra example

--- playground/server/plugins/feed.ts
+++ playground/server/plugins/feed.ts
@@ -14,6 +14,27 @@ export default (nitroApp: NitroApp) => {
     }
   })
 
+  nitroApp.hooks.hook('feedme:handle:content:item[/contentDefaults.xml]', async ({ feed: { insert, invoke, parsed } }) => {
+    // In case if you use several nitro plugins, item can be created already
+    const maybeItemOptions = invoke()
+    // Insertions replace item, so you can update current \ create new with existed keys (undefined equals to {})
+    insert({
+      ...maybeItemOptions,
+      image: {
+        title: 'Title',
+        // parsed.feedme.image.url | parsed.feedme.image.source | parsed.image.url | ...
+        // Depends on how you put this item in yaml meta
+        // Yaml objects (such as image: { title: Title, source: Link }) will be kept
+        //
+        // The most probably is that you have just relative link to this page: media/og.png
+        // so it should be concatenated with parsed._path
+        //
+        // Then you can apply tag [/^(?=\/)/, baseUrl], to replace relative links to hosted ones
+        url: `${parsed._path}/${parsed?.image?.url}`,
+      },
+    })
+  })
+
   nitroApp.hooks.hook('feedme:handle[/feed.xml]', async ({ context: { event }, feed }) => {
     // Note: In case when several hooks exist, they may be called in chain.
     //       So it's possible to completely override early feed
