1e4081e11000593256032e3b4be7e2ab62e8090f
Helltraitor <helltraitor@hotmail.com>
2023-08-26 01:45:19 +0300

    feat: removed feedme module declaration

--- src/virtual.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '#feedme' {
-  // TODO: Rollup works incorrect for exports in shims (https://github.com/Swatinem/rollup-plugin-dts/issues/162)
-  // export default FeedmeModuleOptions
-}
-
-export {}
